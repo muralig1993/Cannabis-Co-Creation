b2256c4802bbfd94865c734a0c480e6a56927cf1
jose31canizar <jose.canizares@colorado.edu>
2017-11-06 20:33:41 -0700

    create xcode project

---
+++ Grasshoppr/Grasshoppr/CreateProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grasshoppr
+//
+//  Created by Jose Canizares on 11/6/17.
+//  Copyright © 2017 Jose Canizares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
